5b6eb3a461a74e50e236fc436e78ea02f9e1b1dd
Nam Nguyen <nguyenvietnam2401@gmail.com>
2020-05-03 23:53:24 +1000

    Updated README.md

--- README.md
+++ README.md
@@ -7,3 +7,24 @@
 - Desktop Framework: Electron.js (Node.js)
 - Data Storage: JSON (assisted by "electron-store" - npm package)
 - Cross-platform app: Windows, MacOS, Linux
+
+### Instructions to run the program:
+1. Download the repository:
+	> git clone https://github.com/nnfunny/vocabulary-cellar.git
+2. Install all needed packages:
+	> npm install
+	
+	or
+	> yarn 
+3.  Optional: If you are using "yarn" please move to step 4. However, if you are using "npm", please jump into package.json, look at "scripts" and the "start" line into the following line:
+	> "start": "concurrently \"cross-env BROWSER=none npm run react-start\" \"wait-on http://localhost:3000 && electron .\""
+4. Run the application locally:
+	> yarn start
+	
+	or
+	> npm run start
+
+The desktop app will pop up to create vocabulary lists.
+
+### The below image shows screenshot of my application:
+
--- public/electron.js
+++ public/electron.js
@@ -7,48 +7,48 @@ const isDev = require("electron-is-dev");
 
 // Reload Electron App
 // if(isDev) {
-    // require("electron-reload")(__dirname, {
-        // electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
-        // hardResetMethod: 'exit',
-    // });
+// require("electron-reload")(__dirname, {
+// electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+// hardResetMethod: 'exit',
+// });
 // }
 
 let mainWindow;
 
 // Update app from Github repository
 // require("update-electron-app")({
-    // repo: "",
-    // updateInterval: "1 hour"
+// repo: "",
+// updateInterval: "1 hour"
 // })
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 1000,
-        height: 775,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-    mainWindow.loadURL(
-        isDev
-            ? "http://localhost:3000"
-            : `file://${path.join(__dirname, "../build/index.html")}`
-    );
-    mainWindow.on("closed", () => (mainWindow = null));
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 775,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  mainWindow.on("closed", () => (mainWindow = null));
 }
 
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-    if(process.platform !== "darwin") {
-        app.quit();
-    }
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 app.on("activate", () => {
-    if(mainWindow === null) {
-        createWindow();
-    }
-})
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
 
-
+// console.log(app.getPath("userData"));
